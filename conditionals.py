66f808bc3320b557040d1dbcbaa4fabd7975bc47
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-07-12 13:49:51 +0500

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -5,4 +5,23 @@ Created on Sun Jul 12 12:49:56 2026
 
 @author: khalievmarufjon
 """
+# 12.07.2026
+
+# task_1
+# Foydalanuvchidan yoshini so'rang: agar 18 dan katta bo'lsa "Kattasiz", aks holda "Voyaga yetmagansiz" deb chiqaring
+
+yosh = int(input(f"Yoshingizni kiriting: "))
+if yosh > 18:
+    print("Siz 18 yoshdan kattasiz")
+else:
+    print("Siz voyaga yetmagansiz")
+    
+# task_2
+# Uchta sonni kiritib, ular orasidan eng kattasini toping (if/elif/else bilan)
+
+
+# Yil kiritib, u kabisa yil ekanligini tekshiruvchi dastur yozing (kabisa yil: 4 ga bo'linadi, lekin 100 ga bo'linsa 400 ga ham bo'linishi kerak)
+# Foydalanuvchidan baholarini so'rang (0-100) va harf bahosini chiqaring (90+ = A, 80-89 = B, va h.k.)
+# Ikki xonali son kiritib, u juft yoki toq ekanligini, shuningdek musbat yoki manfiy ekanligini aniqlang
+
 
