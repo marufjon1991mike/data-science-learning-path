06799e3aa4e42aa5d7f7047a999104568b6a0eb9
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-07-13 18:10:32 +0500

    mini project

---
+++ dictionaries_sets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 18:03:12 2026
+
+@author: khalievmarufjon
+"""
+
+# Listdagi takrorlanuvchi elementlarni olib tashlab, faqat noyob (unique) qiymatlarni qoldiring (list orqali, set() ishlatmasdan)
+# Foydalanuvchidan 5 ta son ketma-ket kiritilishini so'rang, ularni listga saqlang va o'rtachasini hisoblang
+# Ikkita listni birlashtirib (+ yoki extend()), natijani saralab (sort()) chiqaring
+# O'z ma'lumotlaringizni dictionary sifatida saqlang: {"ism": "Kamol", "yosh": 25, "shahar": "Navoiy"}, so'ng har bir kalit-qiymatni alohida chop eting (.items() bilan)
+# Talabalar va ularning baholarini saqlovchi dictionary yarating (masalan, {"Ali": 85, "Vali": 92, "Sami": 78}), so'ng eng yuqori bahoga ega talabani toping
+# Foydalanuvchidan mahsulot nomi va narxini ketma-ket kiritishni so'rang (5 marta), ularni dictionary'ga saqlang, so'ng jami summani hisoblang
+# Ikkita set yarating (masalan, {1,2,3,4,5} va {4,5,6,7,8}), ularning kesishmasi (intersection), birlashmasi (union) va farqini (difference) toping
+# Berilgan matndagi noyob so'zlarni set yordamida aniqlang (masalan, "salom dunyo salom python" dan takrorlanmagan so'zlar ro'yxatini chiqaring)
+
---
+++ mini_project_word_frequency.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 18:09:34 2026
+
+@author: khalievmarufjon
+"""
+
+# Foydalanuvchidan uzun bir matn (bir necha jumla) kiritishini so'rang. Dastur:
+
+# Matnni so'zlarga ajratsin (.split())
+# Har bir so'z necha marta takrorlanganini dictionary'da hisoblasin
+# Eng ko'p takrorlangan 3 ta so'zni chiqarib bersin
