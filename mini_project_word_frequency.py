41e1ef71a1026208055a3cd57f0361a9ed2ebff9
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-07-14 16:35:34 +0500

    Update mini_project_word_frequency.py

--- mini_project_word_frequency.py
+++ mini_project_word_frequency.py
@@ -11,3 +11,44 @@ Created on Mon Jul 13 18:09:34 2026
 # Matnni so'zlarga ajratsin (.split())
 # Har bir so'z necha marta takrorlanganini dictionary'da hisoblasin
 # Eng ko'p takrorlangan 3 ta so'zni chiqarib bersin
+
+matn =""" Python dasturlash tili juda qiziqarli. Python bilan siz ma'lumotlarni tahlil qilishingiz 
+    mumkin. Ma'lumotlar bilan ishlash uchun Python eng yaxshi til hisoblanadi. Data science 
+    sohasida Python juda mashhur. Men Python o'rganishni davom ettirmoqchiman."""
+
+matn = matn.lower() # matn ichidagi har bir so'zni kichik harflarga o'tkazadi
+
+matn = matn.replace(".", "") # tinish belgilarni olib tashlash uchun
+matn = matn.replace(",", "")
+matn = matn.replace("!", "")
+matn = matn.replace("?", "")
+
+sozlar = matn.split() # matndagi har bir so'zni alohida ajratadi
+
+sozlar_soni = {} # bu lug'atda har bir so'z necha marta takrorlanganini saqlaydi
+
+for soz in sozlar:
+    if soz in sozlar_soni:
+        sozlar_soni[soz] += 1
+    else:
+        sozlar_soni[soz] = 1
+
+sozlar_soni_nusxa = sozlar_soni.copy() # asosiy lug'atni yo'qotmaslik uchun uni nusxalaymiz
+
+for i in range(1, 4):
+    eng_kop_soz = None
+    eng_katta_son = 0
+    for soz, son in sozlar_soni_nusxa.items():
+        if son > eng_katta_son:
+            eng_katta_son = son
+            eng_kop_soz = soz
+    print(f"{i}. {eng_kop_soz} - {eng_katta_son} marta")
+    sozlar_soni_nusxa[eng_kop_soz] = 0
+ 
+
+
+
+
+
+
+
