5ff39eda1f923536c3a6faa52c43de74bbc93dc8
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-07-16 20:04:44 +0500

    try-except, with, while

--- shaxsiy xarajatlar hisoblagichi.py
+++ shaxsiy xarajatlar hisoblagichi.py
@@ -22,15 +22,69 @@ Created on Wed Jul 15 16:44:19 2026
 # Xatolarni boshqarish — agar foydalanuvchi summa o'rniga harf kiritsa, dastur qulamasligi kerak
 
 def menyu():
-    print("1. Yangi xarajatlaar qo'shish")
-    print("2. Barcha xarajatlaarni ko'rish")
+    print("1. Yangi xarajatlar qo'shish")
+    print("2. Barcha xarajatlarni ko'rish")
     print("3. Jami xarajatlarni hisoblash")
     print("4. Chiqish")
 
+# Xarajat qo'shish funksiyasi
+
+# Funksiya yozing — nomi va summasini so'rasin, try/except bilan summa xato kiritilishini tekshirsin, 
+# so'ng faylga "a" rejimida yozsin (masalan, "non,5000\n" formatida).
+
+def jami_hisobla():
+    jami = []
+    try:
+        with open("xarajatlar.txt", "r") as fayl:
+            for qator in fayl:
+                nomi, summa = qator.strip().split(",")
+                summa = int(summa)
+                jami.append(summa)
+            print(f"Jami xarajatlar {sum(jami)} so'm bo'ldi.")
+    except FileNotFoundError:
+        print("Hozircha xarajat yo'q!")
+
+        
+def xarajatlarni_korsat():
+    try:
+        with open("xarajatlar.txt", "r") as fayl:
+            for qator in fayl:
+                nomi, summa = qator.strip().split(",") # qator.strip() — qator oxiridagi \n belgisini olib tashlaydi
+                                                    # .split(",") — vergul bo'yicha ikkiga ajratadi: ["kartoshka", "20000"]
+                print(f"{nomi} : {summa} so'm")
+    except FileNotFoundError:
+        print("Hozircha xarajat yo'q!")
+    
+def xarajat_qoshish():
+    nomi = input("Xarajat nomini kiriting: ")
+    try:
+        summa = int(input("Summani kiriting: "))
+        with open("xarajatlar.txt", "a") as fayl:
+            fayl.write(f"{nomi},{summa}\n")
+        print("Xarajat qo'shildi!")
+    except ValueError:
+        print("Iltimos xarajatlarni son bilan kiriting!")
+        
 while True:
     menyu()
     tanlov = input("Tanlovingizni kiriting: ")
     if tanlov == "4":
         print("Dastur yopildi")
         break
+    elif tanlov == "1":
+        xarajat_qoshish()
+    elif tanlov == "2":
+        xarajatlarni_korsat()
+    elif tanlov == "3":
+        jami_hisobla()
+
+
+
+
+            
+    
+
+    
+
+
 
