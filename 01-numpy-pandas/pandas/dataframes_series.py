29ef571bfd49fdaceba4d3c706976eeb6df3b893
Marufjon Khaliev <marufjon1991mike@gmail.com>
2026-07-17 02:39:48 +0500

    Update dataframes_series.py

--- 01-numpy-pandas/pandas/dataframes_series.py
+++ 01-numpy-pandas/pandas/dataframes_series.py
@@ -7,3 +7,47 @@ Created on Thu Jul 16 22:28:59 2026
 """
 
 # 16.07.2026
+
+import pandas as pd
+
+# task_1
+# Yuqoridagi kabi, 4 ta do'stingiz (ism, yosh, shahar) haqida dictionary yarating, undan DataFrame hosil qiling va chop eting.
+
+malumot = {
+    "ism" : ["Ali", "Vali", "Kamol", "Jamol"],
+    "yosh" : [29, 18, 17, 19],
+    "shahar" : ["Navoiy", "Jizzax", "Buxoro", "Toshkent"]
+    }
+df = pd.DataFrame(malumot)
+print(df)
+
+# task_2 DataFrame'ning quyidagi xususiyatlarini sinab ko'ring:
+
+print(df.shape)      # (qatorlar, ustunlar) soni
+print(df.columns)    # ustun nomlari
+print(df.dtypes)     # har bir ustunning ma'lumot turi
+print(df.info())     # umumiy ma'lumot    
+
+# task_3 Faqat bitta ustunni tanlab chop eting:
+
+print(df["ism"])
+
+# task_4 Faqat yosh ustunidagi qiymatlarning o'rtachasini toping (.mean()).
+
+print(df["yosh"].mean())
+
+# task_5 DataFrame'ning birinchi 2 ta qatorini ko'rsating (.head(2)).
+print(df.head(2))
+
+
+
+
+
+
+
+
+
+
+
+
+
